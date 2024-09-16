24164dc3607c5a20f8c94537ec211c7122d2d4d1
martodele703 <mdeleon@agtium.com>
2024-09-16 10:50:44 -0300

    Correccion de clases y directorios

--- Program/AndGate.cs
+++ Program/AndGate.cs
@@ -2,5 +2,23 @@ namespace DefaultNamespace;
 
 public class AndGate: LogicGate
 {
-    
+    private List<LogicGate> entradas;
+
+    public And(List<Compuerta> entradas)
+    {
+        this.entradas = entradas;
+    }
+
+    public override bool Evaluar()
+    {
+        foreach (var entrada in entradas)
+        {
+            if (!entrada.Evaluar())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
